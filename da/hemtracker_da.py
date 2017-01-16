9ef2c317766c9b51eb298c5e548921d8592b62f1
Arkady Zgonnikov <zzaarrkk@gmail.com>
2017-01-16 14:29:34 +0000

    Subject id generation updated

--- da/hemtracker_da.py
+++ da/hemtracker_da.py
@@ -66,15 +66,21 @@ class HEMTrackerDA:
     # this function generates new random subject id if one is not provided in constants.py
     # (which is only the case for experiments requiring multiple sessions with each subject)
     def generate_subj_id(self):
+        file_name = 'existing_subj_ids.txt'
+        try:
+            f = open(file_name, 'r')
+        except IOError:
+            with open(file_name, 'w') as f:
+    
         if SUBJ_ID is None:
-            existing_subj_ids = np.loadtxt('existing_subj_ids.txt')
+            existing_subj_ids = np.loadtxt(file_name)
             subj_id = int(random.uniform(ID_RANGE[0], ID_RANGE[1]))
             while subj_id in existing_subj_ids:
                 subj_id = int(random.uniform(ID_RANGE[0], ID_RANGE[1]))
     
-            with open('existing_subj_ids.txt', 'ab+') as fp:
-                writer = csv.writer(fp, delimiter = '\t')
-                writer.writerow([str(subj_id)])
+            with open(file_name, 'ab+') as fp:
+#                writer = csv.writer(fp, delimiter = '\t')
+#                writer.writerow([str(subj_id)])
         else:
             subj_id = SUBJ_ID
         return str(subj_id)
